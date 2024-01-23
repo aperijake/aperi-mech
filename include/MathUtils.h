57ce7e8ef7133612da7407bc2738906642f00887
Jake Koester <jake.koester@apericmc.com>
2024-01-22 18:54:16 -0700

    added velocity bc test

--- include/MathUtils.h
+++ include/MathUtils.h
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cmath>
+#include <stdexcept>
 #include <vector>
 
 namespace aperi {
@@ -35,9 +36,56 @@ void ChangeLength(T &vector, double new_magnitude) {
 }
 
 // Constant interpolation
-double ConstantInterpolation(double x, const std::vector<double> &abscissa, const std::vector<double> &ordinate);
+template <typename T>
+double ConstantInterpolation(double x, const T &abscissa, const T &ordinate) {
+    if (abscissa.size() != ordinate.size()) {
+        throw std::runtime_error("Abscissa and ordinate vectors must be the same size.");
+    }
+    if (abscissa.size() == 0) {
+        throw std::runtime_error("Abscissa and ordinate vectors must have at least one element.");
+    }
+    if (abscissa.size() == 1) {
+        return ordinate[0];
+    }
+    if (x < abscissa[0]) {
+        return 0.0;
+    }
+    if (x > abscissa[abscissa.size() - 1]) {
+        return ordinate[abscissa.size() - 1];
+    }
+    for (size_t i = 0; i < abscissa.size() - 1; ++i) {
+        if (x >= abscissa[i] && x <= abscissa[i + 1]) {
+            return ordinate[i];
+        }
+    }
+    throw std::runtime_error("Constant interpolation failed.");
+}
 
 // Linear interpolation
-double LinearInterpolation(double x, const std::vector<double> &abscissa, const std::vector<double> &ordinate);
+template <typename T>
+double LinearInterpolation(double x, const T &abscissa, const T &ordinate) {
+    if (abscissa.size() != ordinate.size()) {
+        throw std::runtime_error("Abscissa and ordinate vectors must be the same size.");
+    }
+    if (abscissa.size() == 0) {
+        throw std::runtime_error("Abscissa and ordinate vectors must have at least one element.");
+    }
+    if (abscissa.size() == 1) {
+        return ordinate[0];
+    }
+    if (x < abscissa[0]) {
+        return ordinate[0];
+    }
+    if (x > abscissa[abscissa.size() - 1]) {
+        return ordinate[abscissa.size() - 1];
+    }
+    for (size_t i = 0; i < abscissa.size() - 1; ++i) {
+        if (x >= abscissa[i] && x <= abscissa[i + 1]) {
+            double slope = (ordinate[i + 1] - ordinate[i]) / (abscissa[i + 1] - abscissa[i]);
+            return ordinate[i] + slope * (x - abscissa[i]);
+        }
+    }
+    throw std::runtime_error("Linear interpolation failed.");
+}
 
 }  // namespace aperi
--- test/BoundaryConditionTest.cpp
+++ test/BoundaryConditionTest.cpp
@@ -80,6 +80,120 @@ class BoundaryConditionTest : public ApplicationTest {
         }
     }
 
+    void CheckBoundaryConditions(std::string bc_type) {
+        // Get the boundary condition from the yaml data
+        YAML::Node boundary_condition = m_yaml_data["procedures"][0]["explicit_dynamics_procedure"]["boundary_conditions"];
+
+        // Get the time stepper
+        YAML::Node time_stepper = m_io_input_file->GetTimeStepper(0).begin()->second;
+
+        // Make sure this is using a direct time stepper
+        EXPECT_EQ(m_io_input_file->GetTimeStepper(0).begin()->first.as<std::string>(), "direct_time_stepper");
+
+        // Get the time increment
+        double time_increment = time_stepper["time_increment"].as<double>();
+
+        // Get the final time
+        double final_time = time_stepper["time_end"].as<double>();
+
+        // Check the length of the boundary conditions
+        EXPECT_EQ(m_boundary_conditions.size(), boundary_condition.size());
+        EXPECT_TRUE(boundary_condition.size() > 0);
+
+        // Set up the expected displacement and velocity values
+        std::array<double, 3> expected_displacement = {0.0, 0.0, 0.0};
+        std::array<double, 3> expected_velocity = {0.0, 0.0, 0.0};
+
+        // Initial time
+        for (size_t i = 0; i < m_boundary_conditions.size(); ++i) {
+            m_io_mesh->GetBulkData().update_field_data_states();
+            m_boundary_conditions[i]->Apply(0);
+        }
+
+        for (double time = 0.0; time < final_time; time += time_increment) {
+            // Apply the boundary conditions
+            for (size_t i = 0; i < m_boundary_conditions.size(); ++i) {
+                m_io_mesh->GetBulkData().update_field_data_states();
+                m_boundary_conditions[i]->Apply(time);
+            }
+            UpdateNodalDisplacements(time_increment);
+
+            // Check the boundary conditions
+            for (size_t i = 0; i < m_boundary_conditions.size(); ++i) {
+                // Get boundary condition node
+                YAML::Node bc_node = boundary_condition[i].begin()->second;
+
+                // Get the part and selector for the boundary condition
+                std::vector<std::string> sets = bc_node["sets"].as<std::vector<std::string>>();
+                EXPECT_TRUE(sets.size() > 0);
+
+                // Create a selector for the sets
+                stk::mesh::PartVector parts;
+                for (const auto &set : sets) {
+                    stk::mesh::Part *part = m_io_mesh->GetMetaData().get_part(set);
+                    EXPECT_TRUE(part != nullptr);
+                    parts.push_back(part);
+                }
+                stk::mesh::Selector parts_selector = stk::mesh::selectUnion(parts);
+
+                // Check the selector
+                EXPECT_TRUE(parts_selector == m_boundary_conditions[i]->GetSelector());
+
+                // Get the boundary condition magnitude
+                double magnitude = bc_node["magnitude"].as<double>();
+
+                // Get the boundary condition direction
+                std::array<double, 3> direction = bc_node["direction"].as<std::array<double, 3>>();
+
+                // Get the time function type
+                YAML::Node time_function = bc_node["time_function"];
+                EXPECT_TRUE(time_function.begin()->first.as<std::string>() == "ramp_function") << "Found: " << time_function.begin()->first.as<std::string>();
+                YAML::Node ramp_function = time_function.begin()->second;
+
+                // Check the size of the abscissa and ordinate values
+                EXPECT_TRUE(ramp_function["abscissa_values"].size() == 2);
+                EXPECT_TRUE(ramp_function["ordinate_values"].size() == 2);
+
+                // Get the abscissa values and ordinate values
+                std::array<double, 2> abscissa = ramp_function["abscissa_values"].as<std::array<double, 2>>();
+                std::array<double, 2> ordinate = ramp_function["ordinate_values"].as<std::array<double, 2>>();
+
+                // Compute the ordinate derivative
+                std::array<double, 2> ordinate_derivate;
+                for (size_t i = 0; i < ordinate.size() - 1; ++i) {
+                    ordinate_derivate[i] = (ordinate[i + 1] - ordinate[i]) / (abscissa[i + 1] - abscissa[i]);
+                }
+                ordinate_derivate[ordinate.size() - 1] = 0.0;
+
+                // Interpolate the abscissa and ordinate values to get the time scale factor
+                if (bc_type == "displacement") {
+                    double displacement_time_scale_factor = aperi::LinearInterpolation(time, abscissa, ordinate);  // TODO(jake): LinearInterpolation should be tested
+                    expected_displacement = direction;
+                    aperi::ChangeLength(expected_displacement, magnitude * displacement_time_scale_factor);  // TODO(jake): ChangeLength should be tested
+
+                    double velocity_time_scale_factor = aperi::ConstantInterpolation(time, abscissa, ordinate_derivate);  // TODO(jake): ConstantInterpolation should be tested
+                    expected_velocity = direction;
+                    aperi::ChangeLength(expected_velocity, magnitude * velocity_time_scale_factor);
+                } else if (bc_type == "velocity") {
+                    double velocity_time_scale_factor = aperi::LinearInterpolation(time - time_increment, abscissa, ordinate);  // TODO(jake): Double check this. Should it be time - time_increment due to central difference?
+                    expected_velocity = direction;
+                    aperi::ChangeLength(expected_velocity, magnitude * velocity_time_scale_factor);
+
+                    // Integrate the displacement expected values when the boundary condition is velocity
+                    for (size_t i = 0; i < expected_displacement.size(); ++i) {
+                        expected_displacement[i] = expected_displacement[i] + expected_velocity[i] * time_increment;
+                    }
+                } else {
+                    EXPECT_TRUE(false) << "Boundary condition type must be 'velocity' or 'displacement'. Found: " << bc_type << ".";
+                }
+
+                // Check the displacement and velocity values
+                CheckNodeFieldValues(m_io_mesh->GetBulkData(), parts_selector, "displacement", expected_displacement);
+                CheckNodeFieldValues(m_io_mesh->GetBulkData(), parts_selector, "velocity", expected_velocity);
+            }
+        }
+    }
+
     std::vector<aperi::FieldData> m_field_data;
     std::shared_ptr<aperi::IoInputFile> m_io_input_file;
     std::shared_ptr<aperi::FieldManager> m_field_manager;
@@ -88,7 +202,6 @@ class BoundaryConditionTest : public ApplicationTest {
 
 // Test adding a displacement boundary condition
 TEST_F(BoundaryConditionTest, AddDisplacementBoundaryCondition) {
-
     m_yaml_data = CreateTestYaml();
     AddDisplacementBoundaryConditions(m_yaml_data);
     CreateTestMesh(m_field_manager);
@@ -97,93 +210,20 @@ TEST_F(BoundaryConditionTest, AddDisplacementBoundaryCondition) {
     // Add boundary conditions
     AddTestBoundaryConditions();
 
-    // Get the boundary condition from the yaml data
-    YAML::Node boundary_condition = m_yaml_data["procedures"][0]["explicit_dynamics_procedure"]["boundary_conditions"];
+    // Check the boundary conditions
+    CheckBoundaryConditions("displacement");
+}
 
-    // Get the time stepper
-    YAML::Node time_stepper = m_io_input_file->GetTimeStepper(0).begin()->second;
+// Test adding a velocity boundary condition
+TEST_F(BoundaryConditionTest, AddVelocityBoundaryCondition) {
+    m_yaml_data = CreateTestYaml();
+    AddVelocityBoundaryConditions(m_yaml_data);
+    CreateTestMesh(m_field_manager);
+    CreateInputFile();
 
-    // Make sure this is using a direct time stepper
-    EXPECT_EQ(m_io_input_file->GetTimeStepper(0).begin()->first.as<std::string>(), "direct_time_stepper");
+    // Add boundary conditions
+    AddTestBoundaryConditions();
 
-    // Get the time increment
-    double time_increment = time_stepper["time_increment"].as<double>();
-
-    // Get the final time
-    double final_time = time_stepper["time_end"].as<double>();
-
-    // Check the length of the boundary conditions
-    EXPECT_EQ(m_boundary_conditions.size(), boundary_condition.size());
-    EXPECT_TRUE(boundary_condition.size() > 0);
-
-    std::string bc_type = "displacement";
-
-    // Loop over the time steps
-    for (double time = 0.0; time < final_time; time += time_increment) {
-        // Apply the boundary conditions
-        for (size_t i = 0; i < m_boundary_conditions.size(); ++i) {
-            m_io_mesh->GetBulkData().update_field_data_states();
-            m_boundary_conditions[i]->Apply(time);
-        }
-        UpdateNodalDisplacements(time_increment);
-
-        // Check the boundary conditions
-        for (size_t i = 0; i < m_boundary_conditions.size(); ++i) {
-            // Get boundary condition node
-            YAML::Node bc_node = boundary_condition[i].begin()->second;
-
-            // Get the part and selector for the boundary condition
-            std::vector<std::string> sets = bc_node["sets"].as<std::vector<std::string>>();
-            EXPECT_TRUE(sets.size() > 0);
-
-            // Create a selector for the sets
-            stk::mesh::PartVector parts;
-            for (const auto &set : sets) {
-                stk::mesh::Part *part = m_io_mesh->GetMetaData().get_part(set);
-                EXPECT_TRUE(part != nullptr);
-                parts.push_back(part);
-            }
-            stk::mesh::Selector parts_selector = stk::mesh::selectUnion(parts);
-
-            // Check the selector
-            EXPECT_TRUE(parts_selector == m_boundary_conditions[i]->GetSelector());
-
-            // Get the time function type
-            YAML::Node time_function = bc_node["time_function"];
-            EXPECT_TRUE(time_function.begin()->first.as<std::string>() == "ramp_function") << "Found: " << time_function.begin()->first.as<std::string>();
-            YAML::Node ramp_function = time_function.begin()->second;
-
-            // Get the abscissa values and ordinate values
-            std::vector<double> abscissa = ramp_function["abscissa_values"].as<std::vector<double>>();
-            std::vector<double> ordinate = ramp_function["ordinate_values"].as<std::vector<double>>();
-
-            // Interpolate the abscissa and ordinate values to get the time scale factor
-            double time_scale_factor = 0.0;
-            if (bc_type == "displacement"){
-                time_scale_factor = aperi::LinearInterpolation(time, abscissa, ordinate); // TODO(jake): LinearInterpolation should be tested
-            } else if (bc_type == "velocity") {
-                // Compute the ordinate derivative
-                std::vector<double> ordinate_derivate(ordinate.size());
-                for (size_t i = 0; i < ordinate.size() - 1; ++i) {
-                    ordinate_derivate[i] = (ordinate[i + 1] - ordinate[i]) / (abscissa[i + 1] - abscissa[i]);
-                }
-                ordinate_derivate[ordinate.size() - 1] = 0.0;
-                time_scale_factor = aperi::ConstantInterpolation(time, abscissa, ordinate_derivate);
-            } else {
-                EXPECT_TRUE(false) << "Boundary condition type must be 'velocity' or 'displacement'. Found: " << bc_type << ".";
-            }
-
-            // Get the boundary condition magnitude
-            double magnitude = bc_node["magnitude"].as<double>() * time_scale_factor;
-
-            // Get the boundary condition direction
-            std::array<double, 3> direction = bc_node["direction"].as<std::array<double, 3>>();
-
-            // Reset the length of the direction to match the magnitude
-            aperi::ChangeLength(direction, magnitude);
-
-            // Check the field values
-            CheckNodeFieldValues(m_io_mesh->GetBulkData(), parts_selector, "displacement", direction);
-        }
-    }
+    // Check the boundary conditions
+    CheckBoundaryConditions("velocity");
 }
