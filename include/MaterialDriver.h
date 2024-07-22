eaa6df8f57dd0dae19edb244e3e9a7703176d99f
Jake Koester <jake.koester@apericmc.com>
2024-07-22 22:04:31 +0000

    add a material driver to enable jupyter-based documentation and testing

---
+++ include/MaterialDriver.h
@@ -0,0 +1,139 @@
+#include <yaml-cpp/yaml.h>
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+#include "LogUtils.h"
+#include "Material.h"
+
+namespace aperi {
+
+enum class StressOutputType {
+    FIRST_PIOLA_KIRCHHOFF,
+    SECOND_PIOLA_KIRCHHOFF,
+    CAUCHY
+};
+
+Eigen::Matrix<double, 3, 3> CreateMatrix(const YAML::Node& input_node) {
+    // Grab the matrix node, check if it exists and is a sequence of 3 elements
+    if (!input_node["matrix"]) {
+        throw std::runtime_error("CreateMatrix: Expected a matrix node.");
+    }
+    const YAML::Node& matrix_node = input_node["matrix"];
+
+    if (!matrix_node.IsSequence() || matrix_node.size() != 3) {
+        throw std::runtime_error("CreateMatrix: Expected a sequence of 3 elements for the matrix.");
+    }
+
+    Eigen::Matrix<double, 3, 3> matrix;
+    for (int i = 0; i < 3; ++i) {
+        const YAML::Node& row_node = matrix_node[i];
+        if (!row_node.IsSequence() || row_node.size() != 3) {
+            throw std::runtime_error("CreateMatrix: Expected each row to be a sequence of 3 elements.");
+        }
+        for (int j = 0; j < 3; ++j) {
+            if (!row_node[j]) {
+                throw std::runtime_error("CreateMatrix: Invalid node at matrix[" + std::to_string(i) + "][" + std::to_string(j) + "].");
+            }
+            matrix(i, j) = row_node[j].as<double>();
+        }
+    }
+    return matrix;
+}
+
+std::vector<Eigen::Matrix3d> RunMaterialDriver(YAML::Node input_node) {
+    // Get the data from the input file
+    YAML::Node material_node;
+    YAML::Node displacement_gradients_node;
+    aperi::StressOutputType stress_output_type = aperi::StressOutputType::CAUCHY;
+    try {
+        if (!input_node["material"]) {
+            aperi::CerrP0() << "Material node not found in the input file\n";
+            return {};
+        }
+        material_node = input_node["material"];
+
+        if (!input_node["displacement_gradients"]) {
+            aperi::CerrP0() << "Displacement gradients node not found in the input file\n";
+            return {};
+        }
+        displacement_gradients_node = input_node["displacement_gradients"];
+
+        if (input_node["stress_output"]) {
+            std::string stress_output_type_str = input_node["stress_output"].as<std::string>();
+            std::transform(stress_output_type_str.begin(), stress_output_type_str.end(), stress_output_type_str.begin(), ::toupper);
+            if (stress_output_type_str == "FIRST_PIOLA_KIRCHHOFF") {
+                stress_output_type = aperi::StressOutputType::FIRST_PIOLA_KIRCHHOFF;
+            } else if (stress_output_type_str == "SECOND_PIOLA_KIRCHHOFF") {
+                stress_output_type = aperi::StressOutputType::SECOND_PIOLA_KIRCHHOFF;
+            } else if (stress_output_type_str == "CAUCHY") {
+                stress_output_type = aperi::StressOutputType::CAUCHY;
+            } else {
+                aperi::CerrP0() << "Invalid stress output type: " << stress_output_type_str << "\n";
+                aperi::CerrP0() << "Valid options are: FIRST_PIOLA_KIRCHHOFF, SECOND_PIOLA_KIRCHHOFF, CAUCHY\n";
+                return {};
+            }
+        }
+
+        // Further processing...
+    } catch (const YAML::Exception& e) {
+        aperi::CerrP0() << "Error parsing YAML file: " << e.what() << "\n";
+    }
+
+    // Create a material object from the material node
+    std::shared_ptr<aperi::Material> material = aperi::CreateMaterial(material_node);
+
+    // Get the stress calc functor from the material object
+    aperi::Material::StressFunctor* stress_functor = material->GetStressFunctor();
+
+    // Initialize the stress vector
+    std::vector<Eigen::Matrix3d> stresses(displacement_gradients_node.size());
+
+    // Loop over the displacement gradients
+    size_t i = 0;
+    for (YAML::const_iterator it = displacement_gradients_node.begin(); it != displacement_gradients_node.end(); ++it) {
+        // Get the matrix node from the displacement gradient node
+        YAML::Node matrix_node = it->as<YAML::Node>();
+
+        // Create a displacement gradient matrix from the matrix node
+        Eigen::Matrix<double, 3, 3> displacement_gradient = aperi::CreateMatrix(matrix_node);
+
+        // Run the stress calculation
+        Eigen::Matrix<double, 3, 3> stress = stress_functor->operator()(displacement_gradient);  // First Piola-Kirchhoff stress
+
+        if (stress_output_type == aperi::StressOutputType::SECOND_PIOLA_KIRCHHOFF) {
+            // Convert first Piola-Kirchhoff stress to second Piola-Kirchhoff stress
+            Eigen::Matrix<double, 3, 3> deformation_gradient = displacement_gradient + Eigen::Matrix<double, 3, 3>::Identity();
+            stress = deformation_gradient.inverse() * stress;
+        } else if (stress_output_type == aperi::StressOutputType::CAUCHY) {
+            // Convert first Piola-Kirchhoff stress to Cauchy stress
+            Eigen::Matrix<double, 3, 3> deformation_gradient = displacement_gradient + Eigen::Matrix<double, 3, 3>::Identity();
+            double J = deformation_gradient.determinant();
+            stress = stress * deformation_gradient.transpose() / J;
+        } else if (stress_output_type != aperi::StressOutputType::FIRST_PIOLA_KIRCHHOFF) {
+            aperi::CerrP0() << "Invalid stress output type\n";
+            return {};
+        }
+        aperi::CoutP0() << stress(0, 0) << " " << stress(0, 1) << " " << stress(0, 2) << " "
+                        << stress(1, 0) << " " << stress(1, 1) << " " << stress(1, 2) << " "
+                        << stress(2, 0) << " " << stress(2, 1) << " " << stress(2, 2) << std::endl;
+
+        stresses[i] = stress;
+        ++i;
+    }
+
+    return stresses;
+}
+
+std::vector<Eigen::Matrix3d> RunMaterialDriver(const std::string& input_filename) {
+    YAML::Node input_node;
+    try {
+        input_node = YAML::LoadFile(input_filename);
+    } catch (const YAML::Exception& e) {
+        aperi::CerrP0() << "Error parsing YAML file: " << e.what() << "\n";
+    }
+    return RunMaterialDriver(input_node);
+}
+
+}  // namespace aperi
---
+++ test/MaterialDriverTest.cpp
@@ -0,0 +1,157 @@
+#include <MaterialDriver.h>
+#include <gtest/gtest.h>
+#include <yaml-cpp/yaml.h>
+
+#include <vector>
+
+#include "CaptureOutputTestFixture.h"
+#include "UnitTestUtils.h"
+
+/*
+material:
+    neo_hookean:
+        density: 1
+        youngs_modulus: 4.4989775051124745e+6
+        poissons_ratio: 0.4996591683708248
+stress_output: cauchy
+displacement_gradients:
+    - matrix:
+        - [1.0, 0.0, 0.0]
+        - [0.0, -0.2927726653, 0.0]
+        - [0.0, 0.0, -0.2927726653]
+    - matrix:
+        - [-0.2927726653, 0.0, 0.0]
+        - [0.0, 1.0, 0.0]
+        - [0.0, 0.0, -0.2927726653]
+    - matrix:
+        - [-0.2927726653, 0.0, 0.0]
+        - [0.0, -0.2927726653, 0.0]
+        - [0.0, 0.0, 1.0]
+*/
+
+class MaterialDriverTest : public CaptureOutputTest {
+   protected:
+    void SetUp() override {
+        double bulk_modulus = 2.2e9;  // Pa
+        double mu = 1.5e6;            // Pa
+        double youngs_modulus = 9.0 * mu * bulk_modulus / (3.0 * bulk_modulus + mu);
+        double poissons_ratio = (3.0 * bulk_modulus - 2.0 * mu) / (2.0 * (3.0 * bulk_modulus + mu));
+        // Create the input yaml node
+        m_input_node["material"]["neo_hookean"]["density"] = 1.0;
+        m_input_node["material"]["neo_hookean"]["youngs_modulus"] = youngs_modulus;
+        m_input_node["material"]["neo_hookean"]["poissons_ratio"] = poissons_ratio;
+        m_input_node["stress_output"] = "cauchy";
+        YAML::Node displacement_gradients_node(YAML::NodeType::Sequence);
+
+        // First matrix
+        YAML::Node matrix1(YAML::NodeType::Sequence);
+        matrix1.push_back(YAML::Node(std::vector<double>{1.0, 0.0, 0.0}));
+        matrix1.push_back(YAML::Node(std::vector<double>{0.0, -0.2927726653, 0.0}));
+        matrix1.push_back(YAML::Node(std::vector<double>{0.0, 0.0, -0.2927726653}));
+        YAML::Node matrix_node1;
+        matrix_node1["matrix"] = matrix1;
+        displacement_gradients_node.push_back(matrix_node1);
+
+        // Second matrix (as an example, you would fill in the actual values)
+        YAML::Node matrix2(YAML::NodeType::Sequence);
+        matrix2.push_back(YAML::Node(std::vector<double>{-0.2927726653, 0.0, 0.0}));
+        matrix2.push_back(YAML::Node(std::vector<double>{0.0, 1.0, 0.0}));
+        matrix2.push_back(YAML::Node(std::vector<double>{0.0, 0.0, -0.2927726653}));
+        YAML::Node matrix_node2;
+        matrix_node2["matrix"] = matrix2;
+        displacement_gradients_node.push_back(matrix_node2);
+
+        // Third matrix (as an example, you would fill in the actual values)
+        YAML::Node matrix3(YAML::NodeType::Sequence);
+        matrix3.push_back(YAML::Node(std::vector<double>{-0.2927726653, 0.0, 0.0}));
+        matrix3.push_back(YAML::Node(std::vector<double>{0.0, -0.2927726653, 0.0}));
+        matrix3.push_back(YAML::Node(std::vector<double>{0.0, 0.0, 1.0}));
+        YAML::Node matrix_node3;
+        matrix_node3["matrix"] = matrix3;
+        displacement_gradients_node.push_back(matrix_node3);
+
+        // Finally, add the displacement_gradients_node to the main input node
+        m_input_node["displacement_gradients"] = displacement_gradients_node;
+    }
+
+    YAML::Node m_input_node;
+};
+
+TEST_F(MaterialDriverTest, RunMaterialDriver) {
+    // Run the material driver
+    std::vector<Eigen::Matrix3d> cauchy_stresses = aperi::RunMaterialDriver(m_input_node);
+
+    // Check the stress values
+    ASSERT_EQ(cauchy_stresses.size(), 3);
+
+    // Expected stress values, uniaxial tension in x, then y, then z
+    double expected_stress_val = 5.24795e+06;
+    double tolerance = expected_stress_val * 1e-6;
+    for (size_t k = 0; k < 3; ++k) {
+        for (size_t i = 0; i < 3; ++i) {
+            for (size_t j = 0; j < 3; ++j) {
+                if (i == j && i == k) {
+                    EXPECT_NEAR(cauchy_stresses[k](i, j), expected_stress_val, tolerance);
+                } else {
+                    EXPECT_NEAR(cauchy_stresses[k](i, j), 0.0, tolerance);
+                }
+            }
+        }
+    }
+}
+
+TEST_F(MaterialDriverTest, InvalidMaterialNode) {
+    // Create an input node without the material node
+    YAML::Node input_node;
+    std::vector<Eigen::Matrix3d> cauchy_stresses = aperi::RunMaterialDriver(input_node);
+    EXPECT_TRUE(cauchy_stresses.empty());
+}
+
+// Test different stress output types
+TEST_F(MaterialDriverTest, StressOutputTypes) {
+    // Set the stress output type to first piola kirchhoff
+    m_input_node["stress_output"] = "first_piola_kirchhoff";
+
+    // Set the displacement gradients to be one random matrix
+    Eigen::Matrix3d displacement_gradient = Eigen::Matrix3d::Random();
+    YAML::Node displacement_gradients_node(YAML::NodeType::Sequence);
+    YAML::Node matrix;
+    matrix.push_back(YAML::Node(std::vector<double>{displacement_gradient(0, 0), displacement_gradient(0, 1), displacement_gradient(0, 2)}));
+    matrix.push_back(YAML::Node(std::vector<double>{displacement_gradient(1, 0), displacement_gradient(1, 1), displacement_gradient(1, 2)}));
+    matrix.push_back(YAML::Node(std::vector<double>{displacement_gradient(2, 0), displacement_gradient(2, 1), displacement_gradient(2, 2)}));
+    YAML::Node matrix_node;
+    matrix_node["matrix"] = matrix;
+    displacement_gradients_node.push_back(matrix_node);
+    m_input_node["displacement_gradients"] = displacement_gradients_node;
+
+    // Run the material driver
+    std::vector<Eigen::Matrix3d> first_piola_kirchhoff_stresses = aperi::RunMaterialDriver(m_input_node);
+
+    // Change the stress output type to cauchy
+    m_input_node["stress_output"] = "cauchy";
+
+    // Run the material driver
+    std::vector<Eigen::Matrix3d> cauchy_stresses = aperi::RunMaterialDriver(m_input_node);
+
+    // Change the stress output type to second piola kirchhoff
+    m_input_node["stress_output"] = "second_piola_kirchhoff";
+
+    // Run the material driver
+    std::vector<Eigen::Matrix3d> second_piola_kirchhoff_stresses = aperi::RunMaterialDriver(m_input_node);
+
+    // Covert the first piola kirchhoff stress to cauchy and second piola kirchhoff stresses
+    Eigen::Matrix3d deformation_gradient = displacement_gradient + Eigen::Matrix3d::Identity();
+    double J = deformation_gradient.determinant();
+    Eigen::Matrix3d cauchy_from_first_piola_kirchhoff = 1.0 / J * first_piola_kirchhoff_stresses[0] * deformation_gradient.transpose();
+    Eigen::Matrix3d second_piola_kirchhoff_from_first_piola_kirchhoff = deformation_gradient.inverse() * first_piola_kirchhoff_stresses[0];
+
+    // Check that the stresses are consistent
+    double stress_mag = first_piola_kirchhoff_stresses[0].norm();
+    double tolerance = stress_mag * 1e-6;
+    for (size_t i = 0; i < 3; ++i) {
+        for (size_t j = 0; j < 3; ++j) {
+            EXPECT_NEAR(cauchy_stresses[0](i, j), cauchy_from_first_piola_kirchhoff(i, j), tolerance);
+            EXPECT_NEAR(second_piola_kirchhoff_stresses[0](i, j), second_piola_kirchhoff_from_first_piola_kirchhoff(i, j), tolerance);
+        }
+    }
+}
