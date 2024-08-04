a1b36366436307cb9f8a47099f98d457b028f2a7
github-action-benchmark <github@users.noreply.github.com>
2024-08-04 06:25:28 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 5a82501730f2da85b8fea55396bac847d042eab3

---
+++ dev/bench/data.js
@@ -0,0 +1,45 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1722752728443,
+  "repoUrl": "https://github.com/aperijake/aperi-mech",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Jake Koester",
+            "username": "aperijake",
+            "email": "jake.koester@apericmc.com"
+          },
+          "committer": {
+            "name": "Jake Koester",
+            "username": "aperijake",
+            "email": "jake.koester@apericmc.com"
+          },
+          "id": "5a82501730f2da85b8fea55396bac847d042eab3",
+          "message": "temp skip building for performance pipeline while debugging action",
+          "timestamp": "2024-08-04T05:33:49Z",
+          "url": "https://github.com/aperijake/aperi-mech/commit/5a82501730f2da85b8fea55396bac847d042eab3"
+        },
+        "date": 1722752728175,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Taylor Impact: 1 processors, cpu, 7 x 7 x 21 elements, runtime per increment",
+            "value": 1.24708,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "Taylor Impact: 1 processors, cpu, 14 x 14 x 42 elements, runtime per increment",
+            "value": 9.83286,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "Taylor Impact: 1 processors, cpu, 28 x 28 x 84 elements, runtime per increment",
+            "value": 78.8605,
+            "unit": "milliseconds"
+          }
+        ]
+      }
+    ]
+  }
+}
