9fb09dd8c53e8ebad16d213191c826249b02bca7
github-action-benchmark <github@users.noreply.github.com>
2024-08-04 07:40:37 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 234c36f2bd414f84161c806cc998a1faca3cfdf4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722755712769,
+  "lastUpdate": 1722757237467,
   "repoUrl": "https://github.com/aperijake/aperi-mech",
   "entries": {
     "Benchmark": [
@@ -73,6 +73,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "Taylor Impact: 1 processors, cpu, 28 x 28 x 84 elements, runtime per increment",
             "value": 78.19,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jake Koester",
+            "username": "aperijake",
+            "email": "jake.koester@apericmc.com"
+          },
+          "committer": {
+            "name": "Jake Koester",
+            "username": "aperijake",
+            "email": "jake.koester@apericmc.com"
+          },
+          "id": "234c36f2bd414f84161c806cc998a1faca3cfdf4",
+          "message": "turn back on building",
+          "timestamp": "2024-08-04T06:33:13Z",
+          "url": "https://github.com/aperijake/aperi-mech/commit/234c36f2bd414f84161c806cc998a1faca3cfdf4"
+        },
+        "date": 1722757237135,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Taylor Impact: 1 processors, gpu, 7 x 7 x 21 elements, runtime per increment",
+            "value": 0.906611,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "Taylor Impact: 1 processors, gpu, 14 x 14 x 42 elements, runtime per increment",
+            "value": 0.995646,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "Taylor Impact: 1 processors, gpu, 28 x 28 x 84 elements, runtime per increment",
+            "value": 2.9474,
             "unit": "milliseconds"
           }
         ]
