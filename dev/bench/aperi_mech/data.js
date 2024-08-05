329ba7d0d6c405eb98308353f352c7f4c9d02ce6
github-action-benchmark <github@users.noreply.github.com>
2024-08-05 15:44:27 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 79f1004b234b2f6302be93e04fe75e668f730313

--- dev/bench/aperi_mech/data.js
+++ dev/bench/aperi_mech/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722872156348,
+  "lastUpdate": 1722872667751,
   "repoUrl": "https://github.com/aperijake/aperi-mech",
   "entries": {
     "Benchmark": [
@@ -84,6 +84,48 @@ window.BENCHMARK_DATA = {
             "name": "fem_cpu_np_4_run_time",
             "value": 68.70585340899925,
             "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jake Koester",
+            "username": "aperijake",
+            "email": "jake.koester@apericmc.com"
+          },
+          "committer": {
+            "name": "Jake Koester",
+            "username": "aperijake",
+            "email": "jake.koester@apericmc.com"
+          },
+          "id": "79f1004b234b2f6302be93e04fe75e668f730313",
+          "message": "fix missing double quote [skip ci]",
+          "timestamp": "2024-08-05T14:55:20Z",
+          "url": "https://github.com/aperijake/aperi-mech/commit/79f1004b234b2f6302be93e04fe75e668f730313"
+        },
+        "date": 1722872666895,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "rkpm_gpu_np_1_run_time",
+            "value": 324.6353569460007,
+            "unit": "s"
+          },
+          {
+            "name": "rkpm_gpu_np_1_peak_memory",
+            "value": 14438.3828125,
+            "unit": "MB"
+          },
+          {
+            "name": "fem_gpu_np_1_run_time",
+            "value": 40.54019813999912,
+            "unit": "s"
+          },
+          {
+            "name": "fem_gpu_np_1_peak_memory",
+            "value": 1852.1484375,
+            "unit": "MB"
           }
         ]
       }
