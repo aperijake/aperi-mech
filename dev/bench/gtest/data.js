d81a9725460f003e34c977efa7e7afbb1dc95bda
github-action-benchmark <github@users.noreply.github.com>
2024-08-05 05:12:56 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 4c7007d07028654d306b162272b88243fe399443

--- dev/bench/gtest/data.js
+++ dev/bench/gtest/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722833276744,
+  "lastUpdate": 1722834776681,
   "repoUrl": "https://github.com/aperijake/aperi-mech",
   "entries": {
     "Benchmark": [
@@ -56,6 +56,63 @@ window.BENCHMARK_DATA = {
           {
             "name": "Taylor Impact: 1 processors, cpu, hostname: 5fbb41cf7d8d, 28 x 28 x 84 elements, runtime per increment",
             "value": 78.9503,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jake Koester",
+            "username": "aperijake",
+            "email": "146987853+aperijake@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "4c7007d07028654d306b162272b88243fe399443",
+          "message": "Merge pull request #24 from aperijake/cicd",
+          "timestamp": "2024-08-05T04:08:20Z",
+          "url": "https://github.com/aperijake/aperi-mech/commit/4c7007d07028654d306b162272b88243fe399443"
+        },
+        "date": 1722834776211,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compadre Shape Function Construction: 1 processors, gpu, hostname: 910e4e8d8d68, 48 x 48 x 144 elements, runtime",
+            "value": 0.242396,
+            "unit": "seconds"
+          },
+          {
+            "name": "Compadre Shape Function Construction: 1 processors, gpu, hostname: 910e4e8d8d68, 96 x 96 x 288 elements, runtime",
+            "value": 1.54012,
+            "unit": "seconds"
+          },
+          {
+            "name": "Reproducing Kernel Shape Function Construction: 1 processors, gpu, hostname: 910e4e8d8d68, 48 x 48 x 144 elements, runtime",
+            "value": 0.00764798,
+            "unit": "seconds"
+          },
+          {
+            "name": "Reproducing Kernel Shape Function Construction: 1 processors, gpu, hostname: 910e4e8d8d68, 96 x 96 x 288 elements, runtime",
+            "value": 0.0573971,
+            "unit": "seconds"
+          },
+          {
+            "name": "Taylor Impact: 1 processors, gpu, hostname: 910e4e8d8d68, 7 x 7 x 21 elements, runtime per increment",
+            "value": 0.935559,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "Taylor Impact: 1 processors, gpu, hostname: 910e4e8d8d68, 14 x 14 x 42 elements, runtime per increment",
+            "value": 1.00754,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "Taylor Impact: 1 processors, gpu, hostname: 910e4e8d8d68, 28 x 28 x 84 elements, runtime per increment",
+            "value": 2.94053,
             "unit": "milliseconds"
           }
         ]
