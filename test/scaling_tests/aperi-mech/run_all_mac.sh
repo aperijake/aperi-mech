17493dfe3473611fc5876753611d4cfccb1516fb
Jake Koester <jake.koester@apericmc.com>
2024-10-12 07:47:58 -0600

    add smaller scaling runs

--- test/scaling_tests/aperi-mech/run_all_mac.sh
+++ test/scaling_tests/aperi-mech/run_all_mac.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+./batch_run.sh taylor_bar_1e4_hexes/rkpm
+./batch_run.sh taylor_bar_1e4_hexes/rkpm_nodal
+
+./batch_run.sh taylor_bar_8e4_tets/fem
+./batch_run.sh taylor_bar_8e4_tets/rkpm
+./batch_run.sh taylor_bar_8e4_tets/rkpm_nodal
+
 ./batch_run.sh taylor_bar_1e5_hexes/rkpm
 ./batch_run.sh taylor_bar_1e5_hexes/rkpm_nodal
 ./batch_run.sh taylor_bar_1e5_hexes/rkpm_nodal_two_pass
+
+./batch_run.sh taylor_bar_6e5_tets/fem
+./batch_run.sh taylor_bar_6e5_tets/rkpm
+./batch_run.sh taylor_bar_6e5_tets/rkpm_nodal
 
 ./batch_run.sh taylor_bar_1e6_hexes/rkpm
 ./batch_run.sh taylor_bar_1e6_hexes/rkpm_nodal
 ./batch_run.sh taylor_bar_1e6_hexes/rkpm_nodal_two_pass
 
-./batch_run.sh taylor_bar_1e7_hexes/rkpm
-
-./batch_run.sh taylor_bar_4e7_tets/fem
-./batch_run.sh taylor_bar_4e7_tets/rkpm
-
 ./batch_run.sh taylor_bar_5e6_tets/fem
 ./batch_run.sh taylor_bar_5e6_tets/rkpm
 ./batch_run.sh taylor_bar_5e6_tets/rkpm_nodal
 
-./batch_run.sh taylor_bar_6e5_tets/fem
-./batch_run.sh taylor_bar_6e5_tets/rkpm
-./batch_run.sh taylor_bar_6e5_tets/rkpm_nodal
+./batch_run.sh taylor_bar_1e7_hexes/rkpm
+
+./batch_run.sh taylor_bar_4e7_tets/fem
+./batch_run.sh taylor_bar_4e7_tets/rkpm
---
+++ test/scaling_tests/aperi-mech/run_gpu_azuregpu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_1e4_hexes/rkpm
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_1e4_hexes/rkpm_nodal
+
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_8e4_tets/fem
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_8e4_tets/rkpm
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_8e4_tets/rkpm_nodal
+
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_1e5_hexes/rkpm
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_1e5_hexes/rkpm_nodal
+
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_6e5_tets/fem
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_6e5_tets/rkpm
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_6e5_tets/rkpm_nodal
+
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_1e6_hexes/rkpm
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_1e6_hexes/rkpm_nodal
+
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_5e6_tets/fem
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_5e6_tets/rkpm
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_5e6_tets/rkpm_nodal
+
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_1e7_hexes/rkpm
+
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_4e7_tets/fem
+./batch_run.sh -n 1 -g -e ~/aperi-mech/protego-mech/build/ -p 1 taylor_bar_4e7_tets/rkpm
