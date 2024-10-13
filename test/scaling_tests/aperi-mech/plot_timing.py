468d00ba8d5497f6bc64009b656d5089bc31d99b
Jake Koester <jake.koester@apericmc.com>
2024-10-13 09:30:24 -0600

    add per proc scaling data and script to plot

--- test/scaling_tests/aperi-mech/plot_timing.py
+++ test/scaling_tests/aperi-mech/plot_timing.py
@@ -2,6 +2,26 @@
 """
 Given a log file with performance results, grab the timing data and plot it.
 
+*** Cell Counts ************************************
+       Total   Processor   Processor   Processor
+                 Average         Min         Max  Unbalance%
+----------------------------------------------------
+       19082       19082       19082       19082           0%
+***************************************************
+     Finished building Smoothed Cell Data. Time: 12 ms.
+   Finished Setting up for the Solver. Time: 446 ms
+ - Starting Solver
+*** Node Counts ************************************
+        Type       Total   Processor   Processor   Processor
+                             Average         Min         Max  Unbalance%
+----------------------------------------------------
+       Owned       20736       20736       20736       20736           0%
+     Ghosted           0           0           0           0
+
+     .
+     .
+     .
+
 The relevant part of the file looks like this:
                   25        3.125000e-06             7.945130e-03  Write Field Output
                   25        3.125000e-06             7.945130e-03   End of Simulation
@@ -38,64 +58,201 @@ import pandas as pd
 from matplotlib.ticker import FixedLocator
 
 
+def find_num_processes(i, lines):
+    # Find this line: Running on N processes.
+    match = re.search(r"Running on (\d+) processes.", lines[i])
+    if match:
+        return int(match.group(1))
+    return 0
+
+
+def find_solver_block(i, lines):
+    # Find: Timer Group: Explicit Solver
+    solver_match = re.search(r"Timer Group: Explicit Solver", lines[i])
+    solver_data = []
+    if not solver_match:
+        return solver_data
+
+    # Read the data in the next 10 lines
+    for j in range(i, i + 10):
+        # Find: Total Time: 2.061426e-01 seconds
+        match = re.search(r"Total Time: ([\d\.e\+\-]+) seconds", lines[j])
+        if match:
+            total_time = float(match.group(1))
+            solver_data.append(("Total Time", 1.0, total_time))
+            continue
+
+        # Grab the percentage of time spent in each step from these lines:
+        #           UpdateFieldStates |     2.795900e-05 |         0.0135629%
+        #     ApplyBoundaryConditions |     1.112900e-04 |         0.0539869%
+        #                ComputeForce |     1.887114e-01 |           91.5441%
+        # TimeIntegrationNodalUpdates |     4.480290e-03 |           2.17339%
+        #    CommunicateDisplacements |     2.749249e-03 |           1.33366%
+        #            CommunicateForce |     1.006246e-02 |           4.88131%
+
+        # split the line by "|" and grab the second part
+        match = re.search(
+            r"^\s*(.+)\s*\|\s+([\d\.e\+\-]+)\s*\|\s+([\d\.e\+\-]+)%$", lines[j]
+        )
+        if match:
+            step = match.group(1)
+            time = float(match.group(2))
+            fraction = float(match.group(3)) * 100.0
+            solver_data.append((step, fraction, time))
+
+    return solver_data
+
+
+def find_cell_counts(i, lines):
+    # Find: *** Cell Counts ************************************
+    match = re.search(r"\*\*\* Cell Counts \*+\s*\n", lines[i])
+    cell_counts_data = []
+    if match:
+        # Skip the next 4 lines
+        for j in range(i + 1, i + 5):
+            # Find:       Total   Processor   Processor   Processor
+            # Find:                 Average         Min         Max  Unbalance%
+            match = re.search(
+                r"\s*Total\s+Processor\s+Processor\s+Processor\s*\n", lines[j]
+            )
+            if match:
+                continue
+
+            # Find:       19082      4770.5        4770        4771   0.0104811%
+            match = re.search(
+                r"\s*(\d+)\s+([\d\.e\+\-]+)\s+(\d+)\s+(\d+)\s+([\d\.e\+\-]+)%?\s*\n",
+                lines[j],
+            )
+            if match:
+                total = int(match.group(1))
+                average = float(match.group(2))
+                min_value = int(match.group(3))
+                max_value = int(match.group(4))
+                unbalance = float(match.group(5))
+                cell_counts_data.append(
+                    (total, average, min_value, max_value, unbalance)
+                )
+
+    return cell_counts_data
+
+
+def find_node_counts(i, lines):
+    # Find: *** Node Counts ************************************
+    match = re.search(r"\*\*\* Node Counts \*+\s*\n", lines[i])
+    node_counts_data = []
+    if match:
+        # Skip the next 4 lines
+        for j in range(i + 1, i + 5):
+            # Find:        Type       Total   Processor   Processor   Processor
+            # Find:                              Average         Min         Max  Unbalance%
+            match = re.search(
+                r"\s*Type\s+Total\s+Processor\s+Processor\s+Processor\s*\n", lines[j]
+            )
+            if match:
+                continue
+
+            # Find:        Owned       20736       20736       20736       20736           0%
+            match = re.search(
+                r"\s*(\w+)\s+(\d+)\s+([\d\.e\+\-]+)\s+(\d+)\s+(\d+)\s+([\d\.e\+\-]+)%?\s*\n",
+                lines[j],
+            )
+            if match:
+                node_type = match.group(1)
+                total = int(match.group(2))
+                average = float(match.group(3))
+                min_value = int(match.group(4))
+                max_value = int(match.group(5))
+                unbalance = float(match.group(6))
+                node_counts_data.append(
+                    (node_type, total, average, min_value, max_value, unbalance)
+                )
+    return node_counts_data
+
+
 def read_timing_data(log_file):
     with open(log_file, "r") as f:
         lines = f.readlines()
-    timing_data = []
 
-    num_times_found = 0
-    expected_times = 6
     found_solver_block = False
     found_num_processes = False
+    found_cell_counts = False
+    found_node_counts = False
+    found_all = False
     num_processes = 0
-    for line in lines:
+
+    solver_data = []
+    cell_counts = []
+    node_counts = []
+    for i in range(len(lines)):
         # Stop reading the file after we have found everything we need
         if (
             found_num_processes
             and found_num_processes
-            and num_times_found == expected_times
+            and found_solver_block
+            and found_cell_counts
+            and found_node_counts
         ):
+            print("Found all the necessary data in the log file")
+            found_all = True
             break
 
-        # Find this line: Running on 1 processes.
-        match = re.search(r"Running on (\d+) processes.", line)
-        if match:
-            num_processes = int(match.group(1))
-            found_num_processes = True
-            continue
+        if not found_num_processes:
+            num_processes = find_num_processes(i, lines)
+            if num_processes != 0:
+                print("Num processes:", num_processes)
+                found_num_processes = True
+                continue
+
         if not found_solver_block:
-            # Find: Timer Group: Explicit Solver
-            match = re.search(r"Timer Group: Explicit Solver", line)
-            if match:
+            solver_data = find_solver_block(i, lines)
+            if len(solver_data) > 0:
+                print("Solver data:", solver_data)
                 found_solver_block = True
                 continue
 
-        if found_solver_block:
-            # Find: Total Time: 2.061426e-01 seconds
-            match = re.search(r"Total Time: ([\d\.e\+\-]+) seconds", line)
-            if match:
-                total_time = float(match.group(1))
-                timing_data.append((num_processes, "Total Time", 1.0, total_time))
+        if not found_cell_counts:
+            cell_counts = find_cell_counts(i, lines)
+            if len(cell_counts) > 0:
+                print("Cell counts:", cell_counts)
+                found_cell_counts = True
                 continue
 
-            # Grab the percentage of time spent in each step from these lines:
-            #           UpdateFieldStates |     2.795900e-05 |         0.0135629%
-            #     ApplyBoundaryConditions |     1.112900e-04 |         0.0539869%
-            #                ComputeForce |     1.887114e-01 |           91.5441%
-            # TimeIntegrationNodalUpdates |     4.480290e-03 |           2.17339%
-            #    CommunicateDisplacements |     2.749249e-03 |           1.33366%
-            #            CommunicateForce |     1.006246e-02 |           4.88131%
+        if not found_node_counts:
+            node_counts = find_node_counts(i, lines)
+            if len(node_counts) > 0:
+                print("Node counts:", node_counts)
+                found_node_counts = True
+                continue
 
-            # split the line by "|" and grab the second part
-            match = re.search(
-                r"^\s*(.+)\s*\|\s+([\d\.e\+\-]+)\s*\|\s+([\d\.e\+\-]+)%$", line
-            )
-            if match:
-                step = match.group(1)
-                time = float(match.group(2))
-                fraction = float(match.group(3)) * 100.0
-                timing_data.append((num_processes, step, fraction, time))
-                num_times_found += 1
+    if not found_all:
+        message = (
+            "Failed to find all the necessary data in the log file. File: "
+            + log_file
+            + ". Missing:"
+        )
+        if not found_num_processes:
+            message += " num_processes"
+        if not found_solver_block:
+            message += " solver_block"
+        if not found_cell_counts:
+            message += " cell_counts"
+        if not found_node_counts:
+            message += " node_counts"
+        raise ValueError(message)
+
+    # Grab the cell and node counts data
+    cell_count = cell_counts[0][0]
+    node_count = 0
+    for node_type, total, _average, _min_value, _max_value, _unbalance in node_counts:
+        if node_type == "Owned":
+            node_count += total
+
+    timing_data = []
+    # Add the solver, cell and node counts data to the timing data
+    for step, fraction, time in solver_data:
+        timing_data.append(
+            (num_processes, cell_count, node_count, step, fraction, time)
+        )
 
     return timing_data
 
@@ -186,6 +343,45 @@ def plot_timing_data(df, output_dir):
     plt.grid()
     plt.legend()
     plt.savefig(output_dir + os.path.sep + "timing_data_relative.png")
+    plt.close()
+
+
+def plot_timing_data_per_proc(df, output_dir):
+    # Create a plot of the timing data per processor
+    # Want a line for each number of processes
+    # Y-axis is the relative time processing per processor: df["time"] * df["num_procs"] / time_1_cpu
+    # X-axis is the number of cells per processor: df["cell_count_per_proc"]
+    # Do only for the "Total Time" step
+    # Also write a file with the scaling data
+    total_time_data = df[df["step"] == "Total Time"]
+    time_1_cpu = total_time_data[total_time_data["num_procs"] == 1]["time"].values[0]
+
+    output = "num_procs,cell_count_per_proc,scaling_efficiency\n"
+    for num_procs in total_time_data["num_procs"].unique():
+        num_procs_data = total_time_data[total_time_data["num_procs"] == num_procs]
+        scaling_efficiency = num_procs_data["time"] * num_procs / time_1_cpu
+        # Plot log x and linear y
+        plt.semilogx(
+            num_procs_data["cell_count_per_proc"],
+            scaling_efficiency,
+            label=f"{num_procs} processes",
+            marker="o",
+        )
+        for cell_count_per_proc, scaling in zip(
+            num_procs_data["cell_count_per_proc"], scaling_efficiency
+        ):
+            output += f"{num_procs},{cell_count_per_proc},{scaling}\n"
+    # plot a horizontal line at y=1, perfect scaling
+    plt.axhline(y=1, color="k", linestyle="--", label="Perfect Scaling")
+    plt.xlabel("Number of Cells per Processor")
+    plt.ylabel("Time / Time 1 CPU * Number of Processes")
+    plt.legend()
+    plt.grid()
+    plt.savefig(output_dir + os.path.sep + "timing_data_per_proc.png")
+    plt.close()
+
+    with open(output_dir + os.path.sep + "scaling_data.csv", "w") as f:
+        f.write(output)
 
 
 def print_scaling(df):
@@ -251,7 +447,15 @@ if __name__ == "__main__":
             # Create a DataFrame with the timing data
             dfs_for_file.append(
                 pd.DataFrame(
-                    timing_data, columns=["num_procs", "step", "fraction", "time"]
+                    timing_data,
+                    columns=[
+                        "num_procs",
+                        "cell_count",
+                        "node_count",
+                        "step",
+                        "fraction",
+                        "time",
+                    ],
                 )
             )
             # Divide the time and fraction columns by the number of folders
@@ -263,6 +467,10 @@ if __name__ == "__main__":
             file_df["time"] += df_for_file["time"]
             file_df["fraction"] += df_for_file["fraction"]
 
+        # Add the cell_count_per_proc and node_count_per_proc columns
+        file_df["cell_count_per_proc"] = file_df["cell_count"] / file_df["num_procs"]
+        file_df["node_count_per_proc"] = file_df["node_count"] / file_df["num_procs"]
+
         # Add this file's data to the overall DataFrame
         df = pd.concat([df, file_df], ignore_index=True)
 
@@ -272,4 +480,5 @@ if __name__ == "__main__":
 
     # print(df)
     plot_timing_data(df, args.output)
+    plot_timing_data_per_proc(df, args.output)
     print_scaling(df)
