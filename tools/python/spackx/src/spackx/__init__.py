e20fb9c1a2143b9ef05e407b189f65b43176e1fb
Tim Fuller <tjfulle@sandia.gov>
2025-06-05 09:29:33 -0600

    Adding larger spack ecosystem

---
+++ tools/python/spackx/src/spackx/__init__.py
@@ -0,0 +1,16 @@
+import importlib.resources
+import os
+
+def setup_extension() -> str | None:
+    """Set up the aperi spack extensions
+
+    Spack expects the directory name of the configuration path to be returned
+    We also set the variable spackx which is used to locate extension resources
+    
+    """
+    spackx = importlib.resources.files("spackx")
+    os.environ["spackx"] = str(spackx)
+    etc = spackx.joinpath("etc")
+    if etc.is_dir():
+        return str(etc)
+    return None
