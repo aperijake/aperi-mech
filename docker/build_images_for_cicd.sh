9845e412bd360f1c0e1d7ca4e05ae688193967c4
Jake Koester <jake.koester@apericmc.com>
2025-07-12 15:56:29 -0600

    add option to specify branch in dockerfiles

--- docker/build_images_for_cicd.sh
+++ docker/build_images_for_cicd.sh
@@ -3,6 +3,7 @@
 WITH_PROTEGO=false
 USE_CACHE=true
 USE_GPU=false
+SRC_BRANCH="main"
 
 while [[ $# -gt 0 ]]; do
 	case "$1" in
@@ -18,13 +19,17 @@ while [[ $# -gt 0 ]]; do
 		USE_GPU=true
 		shift
 		;;
+	--branch)
+		SRC_BRANCH="$2"
+		shift 2
+		;;
 	*)
 		shift
 		;;
 	esac
 done
 
-if $WITH_PROTEGO; then
+if "${WITH_PROTEGO}"; then
 	export DOCKER_BUILDKIT=1
 	SSH_FLAG="--ssh default"
 	PROTEGO_ARG="--build-arg PROTEGO=1"
@@ -34,13 +39,13 @@ else
 	PROTEGO_ARG=""
 fi
 
-if $USE_CACHE; then
+if "${USE_CACHE}"; then
 	CACHE_FLAG=""
 else
 	CACHE_FLAG="--no-cache"
 fi
 
-if $USE_GPU; then
+if "${USE_GPU}"; then
 	DOCKERFILE="Dockerfile_AperiMech_Ubuntu2404_Nvidia"
 	IMAGE_TAG="aperi-mech:cuda-t4"
 else
@@ -48,12 +53,25 @@ else
 	IMAGE_TAG="aperi-mech:latest"
 fi
 
-docker buildx build \
-	$SSH_FLAG \
-	$CACHE_FLAG \
-	$PROTEGO_ARG \
-	--platform linux/amd64 \
-	--load \
-	-t $IMAGE_TAG \
-	-f $DOCKERFILE . \
-	--progress=plain 2>&1 | tee build_log_$(date +%Y%m%d_%H%M%S).log
+BUILD_ARGS=(--build-arg SRC_BRANCH="${SRC_BRANCH}")
+
+LOG_FILE="build_log_$(date +%Y%m%d_%H%M%S).log"
+
+cmd=(
+	docker buildx build
+	${SSH_FLAG:+${SSH_FLAG}}
+	${CACHE_FLAG:+${CACHE_FLAG}}
+	${PROTEGO_ARG:+${PROTEGO_ARG}}
+	"${BUILD_ARGS[@]}"
+	--platform linux/amd64
+	--load
+	-t "${IMAGE_TAG}"
+	-f "${DOCKERFILE}"
+	.
+	--progress=plain
+)
+
+printf '%q ' "${cmd[@]}"
+# echo "| tee ${LOG_FILE}"
+#
+# "${cmd[@]}" 2>&1 | tee "${LOG_FILE}"
