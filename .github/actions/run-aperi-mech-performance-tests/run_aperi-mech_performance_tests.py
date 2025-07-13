8dad3e4274bf3f7a32bfb18c1ecca770f04f7dd0
Jake Koester <jake.koester@apericmc.com>
2025-07-12 21:29:33 -0600

    fix spack env activate issue in dockers

--- .github/actions/run-aperi-mech-performance-tests/run_aperi-mech_performance_tests.py
+++ .github/actions/run-aperi-mech-performance-tests/run_aperi-mech_performance_tests.py
@@ -43,7 +43,6 @@ def run_aperi_mech_performance_tests(vm_ip, vm_username, gpu, parallel):
 
     docker-compose -f {compose_file} run --rm {service_name} /bin/bash -c '
         echo "Setting up Spack environment..."
-        source $aperi_mech/venv/bin/activate
         spack env activate aperi-mech
 
         echo "Running aperi-mech performance tests..."
