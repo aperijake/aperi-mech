eac92e2217057ab5a6214c2eb45f63beb72feb1e
Jake Koester <jake.koester@apericmc.com>
2025-09-05 10:04:38 -0600

    remove netcdf variant flag for trilinos as it doesnt exist

--- tools/python/spackx/src/spackx/repos/aperi/packages/trilinos/package.py
+++ tools/python/spackx/src/spackx/repos/aperi/packages/trilinos/package.py
@@ -1,6 +1,4 @@
-from spack.package import conflicts
-from spack.package import depends_on
-from spack.package import variant
+from spack.package import conflicts, depends_on, variant
 from spack.pkg.aperi.git_package import GitPackage
 from spack.pkg.builtin.trilinos import Trilinos as builtin_trilinos
 
@@ -8,7 +6,6 @@ from spack.pkg.builtin.trilinos import Trilinos as builtin_trilinos
 class Trilinos(GitPackage, builtin_trilinos):
     variant("krino", default=True)
     depends_on("yaml-cpp", when="+krino")
-    conflicts("~netcdf", when="+krino")
     conflicts("~hdf5", when="+krino")
     conflicts("~mpi", when="+krino")
 
@@ -16,7 +13,9 @@ class Trilinos(GitPackage, builtin_trilinos):
         args = super().cmake_args()
         if self.spec.satisfies("+krino"):
             args.append("-DTPL_ENABLE_yamlcpp:BOOL=ON")
-            args.append(f"-Dyamlcpp_INCLUDE_DIRS={self.spec['yaml-cpp'].prefix.include}")
+            args.append(
+                f"-Dyamlcpp_INCLUDE_DIRS={self.spec['yaml-cpp'].prefix.include}"
+            )
             args.append(f"-Dyamlcpp_LIB_DIRS={self.spec['yaml-cpp'].prefix.lib}")
             args.append("-DTrilinos_ENABLE_Krino:BOOL=ON")
             args.append("-DKrino_ENABLE_TESTS:BOOL=ON")
