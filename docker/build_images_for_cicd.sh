18b965e8c27613912228d0cce63508baa32fa538
Jake Koester <jake.koester@apericmc.com>
2025-10-24 19:42:50 -0600

    update rocky docker

--- docker/build_images_for_cicd.sh
+++ docker/build_images_for_cicd.sh
@@ -5,6 +5,7 @@ USE_CACHE=true
 USE_GPU=false
 SRC_BRANCH="main"
 PLATFORM="amd64"
+DISTRO="ubuntu"
 
 while [[ $# -gt 0 ]]; do
 	case "$1" in
@@ -28,6 +29,10 @@ while [[ $# -gt 0 ]]; do
 		PLATFORM="$2"
 		shift 2
 		;;
+	--distro)
+		DISTRO="$2"
+		shift 2
+		;;
 	--help | -h)
 		echo "Usage: build_images_for_cicd.sh [OPTIONS]"
 		echo "Options:"
@@ -36,6 +41,7 @@ while [[ $# -gt 0 ]]; do
 		echo "  --gpu               Build GPU-enabled image"
 		echo "  --branch BRANCH     Specify the source branch (default: main)"
 		echo "  --platform PLATFORM Specify the platform (default: amd64)"
+		echo "  --distro DISTRO     Specify the distribution: ubuntu or rocky (default: ubuntu)"
 		exit 0
 		;;
 	*)
@@ -61,11 +67,20 @@ else
 fi
 
 if "${USE_GPU}"; then
+	if [[ ${DISTRO} == "rocky" ]]; then
+		echo "Error: GPU build not yet supported for ${DISTRO}"
+		exit 1
+	fi
 	DOCKERFILE="Dockerfile_AperiMech_Ubuntu2404_Nvidia"
 	IMAGE_TAG="aperi-mech:cuda-t4"
 else
-	DOCKERFILE="Dockerfile_AperiMech_Ubuntu2404"
-	IMAGE_TAG="aperi-mech:latest"
+	if [[ ${DISTRO} == "rocky" ]]; then
+		DOCKERFILE="Dockerfile_AperiMech_RockyLinux9"
+		IMAGE_TAG="aperi-mech:rocky9"
+	else
+		DOCKERFILE="Dockerfile_AperiMech_Ubuntu2404"
+		IMAGE_TAG="aperi-mech:latest"
+	fi
 fi
 
 BUILD_ARGS=(--build-arg SRC_BRANCH="${SRC_BRANCH}")
